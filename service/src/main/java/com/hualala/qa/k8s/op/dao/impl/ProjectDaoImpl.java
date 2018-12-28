d0988ffd19d01168d0a7c02b9f2a3326d0e60de5
yefei <yefei@hualala.com>
2018-12-28 11:22:15 +0800

    jar

--- service/src/main/java/com/hualala/qa/k8s/op/dao/impl/ProjectDaoImpl.java
+++ service/src/main/java/com/hualala/qa/k8s/op/dao/impl/ProjectDaoImpl.java
@@ -65,7 +65,7 @@ public class ProjectDaoImpl implements IProjectDao {
     public TblPreServiceStatus getPreServiceStatus(int id){
         DatabaseContextHolder.setDatabaseType(DatabaseType.db_pre_k8s_op);
         TblPreServiceStatus item = tblPreServiceStatusMapper.selectByPrimaryKey(id);
-        item.setApmAgent(String.format("-javaagent:/home/agent/kepler-agent-bootstrap-0.0.1-SNAPSHOT.jar=kepler.agent.application.name=%s:kepler.agent.tier.name=%s:kepler.agent.onthefly=true", item.getBusiness(), item.getServiceName()));
+        item.setApmAgent(String.format("-javaagent:/home/agent/kepler-agent-bootstrap-0.0.1-SNAPSHOT.jar=kepler.agent.application.name=%s:kepler.agent.tier.name=%s", item.getBusiness(), item.getServiceName()));
         return item;
     }
 
@@ -90,7 +90,7 @@ public class ProjectDaoImpl implements IProjectDao {
 
     private void genAgentCode(List<TblPreServiceStatus> list){
         list.forEach(item->{
-            item.setApmAgent(String.format("-javaagent:/home/agent/kepler-agent-bootstrap.jar=kepler.agent.application.name=%s:kepler.agent.tier.name=%s:kepler.agent.onthefly=true", item.getBusiness(), item.getServiceName()));
+            item.setApmAgent(String.format("-javaagent:/home/agent/kepler-agent-bootstrap.jar=kepler.agent.application.name=%s:kepler.agent.tier.name=%s", item.getBusiness(), item.getServiceName()));
         });
     }
 }
