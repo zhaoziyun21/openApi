c5ffd8665debf303c24cc83e72ee56a5142e60a5
yefei <yefei@hualala.com>
2019-03-01 20:34:32 +0800

    删除不用的文件

--- service/src/main/java/com/hualala/api/ServiceApplication.java
+++ service/src/main/java/com/hualala/api/ServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.hualala.api"})
 public class ServiceApplication extends SpringBootServletInitializer {
